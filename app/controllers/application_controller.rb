0f511a81bccb69ed462d287eb65f8c8e514cb000
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-07-26 17:43:39 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  ## ここから追加
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_USER"] && password == ENV["BASIC_PASSWORD"]
+    end
+  end
+  ## ここまで
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
-const {
-  environment
-} = require('@rails/webpacker')
-
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
+const { environment } = require('@rails/webpacker')
 
 module.exports = environment
